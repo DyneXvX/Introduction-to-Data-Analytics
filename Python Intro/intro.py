c4eaf158bed26c4d5bd159a8ba5fdf03141e3441
Justn Thoms <DyneXvX@outlook.com>
2021-01-14 15:54:24 -0500

    setting up intro to python

---
+++ Python Intro/intro.py
@@ -0,0 +1,17 @@
+# %%
+# Slide no. 16
+# Our First Function: print
+
+print('Hello World!')
+print("Hello World!")
+# %%
+for i in [1, 2, 3, 4, 5]:
+    print(i)
+
+    for j in [1, 2, 3, 4, 5]:
+        print(j)
+        print(i + j)
+        print(i)
+
+print("Done with Loop")
+# %%
