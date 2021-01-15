0d3d3b4005340a65c18632ba2ce4eadefd527b49
Justn Thoms <DyneXvX@outlook.com>
2021-01-15 15:53:23 -0500

    Bad storm coming in - backing up now

--- Python Intro/intro.py
+++ Python Intro/intro.py
@@ -15,3 +15,11 @@ for i in [1, 2, 3, 4, 5]:
 
 print("Done with Loop")
 # %%
+# Slide number 20
+
+a = 5; b = 6;  c = 7; print(a)
+# this is all about putting them in the same row
+d = 8
+e = 12
+print (e)
+# %%
