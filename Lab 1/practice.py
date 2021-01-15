9974b604bcaff0ba06548a5d1a26a2eec7f9a553
Justn Thoms <DyneXvX@outlook.com>
2021-01-15 17:04:35 -0500

    lab 1 practice and finished files

---
+++ Lab 1/practice.py
@@ -0,0 +1,54 @@
+#%%
+print('cap')
+print()
+print('4784')
+
+# %%
+print('Justin\nThoms')
+print('\n---\n')
+print('I am learning\n')
+print('Python')
+print('\n---\n')
+print('I am new to')
+print('Python')
+print('\n---\n')
+print('data')
+print('\n')
+print('science')
+# %%
+print('data')
+print('')
+print('analytics')
+# %%
+print('CAP', 4784)
+print('CAP', 4784, sep=' ')
+print('CAP', 4784, sep=None)
+# %%
+# To remove the space
+print('CAP', 4784, sep='')
+# the trick here is to remove the space in the sep
+# %%
+# home/user/documents
+print('home', 'user', 'documents', sep='/')
+# /home/user/documents
+print('/home', 'user', 'documents', sep='/')
+# %%
+print('|', '*', 'Justin', '*','*','*','*','*','*', '*', '*', '*', '*', '*', '*', sep='-', end='--|\n')
+print('|', '*', 'Thoms', '*','*','*','*','*', '*', '*', '*', '*', '*', '*', '*', '*', sep='-', end='-|\n')
+print('|', '*', '*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*', sep='-', end='-|\n')
+print('|', '*', 'Studying Python', '*','*','*','*','*', '*', '*', '*', sep='-', end='-|\n')
+print('|', '*', '*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*', sep='-', end='-|\n')
+print('|', '*', 'at the', '*','*','*','*','*', '*', '*', '*', '*', '*', '*', '*',sep='-', end='--|\n')
+print('|', '*', '*','*','*','*','*','*','*','*','*','*','*','*','*','*','*','*', sep='-', end='-|\n')
+print('|', '*', 'University of North Florida', '*','*', sep='-', end='-|')
+# %%
+print('|', '*', 'Justin ', '*', '*', '*', '  *', '*', '', sep='     ', end='|\n')
+print('|', '*', 'Thoms  ', '*', '*', '*', '  *', '*', '', sep='     ', end='|\n')
+print('|', '*', '*', '*', '*', '*', '*', '  *', '*','', sep='     ', end='|\n')
+print('|', '*', 'Studying Python   *', '*', '  *', '*', '', sep='     ', end='|\n')
+print('|', '*', '*', '*', '*', '*', '*', '  *', '*','', sep='     ', end='|\n')
+print('|', '*', 'at the', ' *', '*', '*', '  *', '*', '', sep='     ', end='|\n')
+print('|', '*', '*', '*', '*', '*', '*', '  *', '*','', sep='     ', end='|\n')
+print('|', '*', 'University of North Florida', '*', '*', '', sep='     ', end='|')
+
+# %%
