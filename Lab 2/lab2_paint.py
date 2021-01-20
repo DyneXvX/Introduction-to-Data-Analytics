aaa2208069d62b24900b5bbb1610bcb2488bceb8
Justn Thoms <DyneXvX@outlook.com>
2021-01-20 17:52:03 -0500

    setup lab 2

---
+++ Lab 2/lab2_paint.py
@@ -0,0 +1,4 @@
+def displayText():
+    print('Hello World from paint.py!')
+
+
---
+++ Lab 2/lab2_paintandbmicalculator.py
@@ -0,0 +1,14 @@
+from lab2_bmi import *
+from lab2_paint import *
+print('Welcome to Python program.')
+print('---------------------------')
+print('Your menu options are: ')
+print('     ', 'Enter 1 for calculating gallons of poiant needed to paint a room.')
+print('     ', 'Enter 2 for calculating Body Mass Index')
+print('     ', 'Enter any other value to quit the program.')
+print('')
+print('Enter your menu option: ')
+
+displayText()
+print('')
+hello()
