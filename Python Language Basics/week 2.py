e7193b78f20efad735bca9e9741741e377a0bbde
Justn Thoms <DyneXvX@outlook.com>
2021-01-21 15:09:58 -0500

    week 2 notes

---
+++ Python Language Basics/week 2.py
@@ -0,0 +1,13 @@
+#%%
+
+# variables and objects
+x = [1,3,5]
+y = x
+x[0] = 2
+print(y)
+# what will y be?
+#%%
+number_of_planets = 9
+print(type(number_of_planets))
+# what type will number_of_planets be?
+# %%
