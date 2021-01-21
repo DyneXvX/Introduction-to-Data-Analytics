7246446b551783aff985fcf325c1f91660f71215
Justn Thoms <DyneXvX@outlook.com>
2021-01-21 11:25:57 -0500

    updated while canvas is down.

--- Lab 2/lab2_paint.py
+++ Lab 2/lab2_paint.py
@@ -1,4 +1,17 @@
-def displayText():
-    print('Hello World from paint.py!')
+def paint():
+    print('Welcome to the paint needed calculator.')
+    print('Enter the length of the room: ')
+    print('Enter the width of the room: ')
+    print('Enter the height of the room: ')
+    print('How many doors are in the room? ')
+    print('How many windows are in the room? ')
 
 
+# For calculating the paint needed, the program should calculate the amount of paint needed to paint the walls of a room of the given length and width. You may use the following assumptions:
+
+# Paint covers 315 square feet per gallon
+# Each door in the room is 25 square feet and each window is 10 square feet.
+# Rooms may contain four walls. Each wall can be considered a rectangle. The total area needed to paint the walls would area of the rectangle.
+# 2 walls in the room will be a product of width and height, and the other 2 walls would be a product of the length and height of the room.
+# Subtract door surface area and window area from the total area of the wall to be painted
+# To calculate the volume of paint needed, you will have to divide the total area to be painted by the area that can be covered for a gallon.
--- Lab 2/lab2_paintandbmicalculator.py
+++ Lab 2/lab2_paintandbmicalculator.py
@@ -2,6 +2,7 @@ from lab2_bmi import *
 from lab2_paint import *
 print('Welcome to Python program.')
 print('---------------------------')
+print('')
 print('Your menu options are: ')
 print('     ', 'Enter 1 for calculating gallons of poiant needed to paint a room.')
 print('     ', 'Enter 2 for calculating Body Mass Index')
@@ -9,6 +10,6 @@ print('     ', 'Enter any other value to quit the program.')
 print('')
 print('Enter your menu option: ')
 
-displayText()
+paint()
 print('')
-hello()
+bmi()
