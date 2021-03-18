3e828913df14c26a9bfec99675cd635dd03c1fc8
Justn Thoms <DyneXvX@outlook.com>
2021-03-18 13:47:01 -0400

    income numbers done

--- Project/main.py
+++ Project/main.py
@@ -3,18 +3,30 @@ import pandas as pd
 import numpy as np
 
 fl_population = np.genfromtxt('DataSets/population.csv', dtype=str, delimiter=',', skip_header=2)
-
 population_frame = pd.DataFrame(fl_population, columns=['County', '2015', '2016', '2017', '2018', '2019'])
-print(population_frame)
 
 average_income = np.genfromtxt('DataSets/Dataset 6.csv', dtype=str, delimiter=',', skip_header=2)
-
 income_frame = pd.DataFrame(average_income, columns=['County', '2015', '2016', '2017', '2018', '2019'])
-print(income_frame)
 
 death_rates = np.genfromtxt('DataSets/deathRates.csv', dtype=str, delimiter=',' , skip_header=1)
 death_frame = pd.DataFrame(death_rates, columns=['County', '2015', '2016', '2017', '2018', '2019'])
 
-print(death_frame)
-# %%
 
+# %%
+#Population Numbers
+population_frame[['2015', '2016', '2017', '2018', '2019']] = population_frame[['2015', '2016', '2017', '2018', '2019']].apply(pd.to_numeric)
+sum_column = population_frame['2015'] + population_frame['2016'] + population_frame['2017'] + population_frame['2018'] + population_frame['2019']
+average = sum_column / 5
+population_frame['Average'] = average
+print('Total population per county with their average over the course of 5 years.')
+print()
+print(population_frame)
+# %%
+#Income Numbers
+income_frame[['2015', '2016', '2017', '2018', '2019']] = income_frame[['2015', '2016', '2017', '2018', '2019']].apply(pd.to_numeric)
+sum_column = income_frame['2015'] + income_frame['2016'] + income_frame['2017'] + income_frame['2018'] + income_frame['2019']
+average = sum_column / 5
+income_frame['Average'] = average
+print('Average income per county with their average over the course of 5 years.')
+print()
+print(income_frame)
